1188bd9068c63039397db88fe58678c705185982
idk <hankhill19580@gmail.com>
2022-06-26 20:51:13 -0400

    update buildscript

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 #! /usr/bin/env sh
-VERSION=$(grep I2P_VERSION ../i2p.android.base/gradle.properties)
+. ../i2p.android.base/gradle.properties
 DATE=$(date +%Y%m%d)
 
 cd ../i2p.i2p && \
     git checkout master; git pull --all && \
     ant distclean mavenLocal
-echo "${VERSION} ${DATE}"
+    ./installer/resources/maven-dev-release.sh 1
+echo "${I2P_VERSION} ${DATE}"
 sleep 20s
-
 cd ../i2p.android.base && \
     git checkout master; git pull origin master
+sed -i.bak "s/I2P_VERSION=.*/I2P_VERSION=${I2P_VERSION}-1/g" gradle.properties
 if [ -f routerjars/local.properties.bak ]; then
     cp -v routerjars/local.properties routerjars/local.properties.bak
 fi
@@ -18,3 +19,4 @@ echo "i2psrc=${HOME}/Workspace/GIT_WORK/i2p.i2p" > routerjars/local.properties
 if [ -f routerjars/local.properties.bak ]; then
     cp -v routerjars/local.properties.bak routerjars/local.properties
 fi
+cp -v gradle.properties.bak gradle.properties
